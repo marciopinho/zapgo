7adb3abee3dddaefdd0d2f63226f10f1b70add9a
Mars Pinho <marciopinho@gmail.com>
2022-09-20 18:50:51 -0300

    index.js back with sendMessage and eventListener

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello from src/index.js!");
+const phoneForm = document.getElementById('phone-form');
+
+const sendMessage = (phone) => {
+  window.open(`https://wa.me/55${phone}`);
+};
+
+phoneForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const phone = document.getElementById('inputPhone').value.replace(/\D/g, '');
+  sendMessage(phone);
+});
